18981889bef8297182ec882319d59960fd5201e9
Harshita Bisht <harshitab574@.com>
2026-08-20 21:14:34 +0530

    Configure Django for production

--- leavesystem/settings.py
+++ leavesystem/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l!l+hru2r+8wklt2k$swlk)07w7uw$(tfrw-yf^1t(3c5kl43q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
