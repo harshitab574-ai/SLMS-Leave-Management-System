cd3b5782e947a959630c99fbb168018d78ff3cd2
Harshita Bisht <harshitab574@.com>
2026-08-22 12:55:38 +0530

    Configure PostgreSQL for deployment

--- leavesystem/settings.py
+++ leavesystem/settings.py
@@ -74,15 +74,13 @@ WSGI_APPLICATION = 'leavesystem.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import os
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'slms',
-        'USER' : 'root',
-        'PASSWORD':'Root@1234',
-        'HOST': 'localhost',
-        'PORT':'3306'
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
 }
 
 # Password validation
